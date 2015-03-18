f8b1c760607e595617e49fdf5e1d3c13e0dba04d
Brendan <brendanheater@gmail.com>
2015-03-18 13:57:27 +0100

    Testing

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Changing something
+## To see if it works ... like a stuid idiot
 
 ## Write a short comment describing this function
 
